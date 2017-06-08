8175b718ca6ed8446be83e2f5eec743122e6f3de
Alex Yang <aleozlx@gmail.com>
2017-06-08 00:11:13 -0500

    init mem.cu

--- bench/mem.cu
+++ bench/mem.cu
@@ -1,14 +1,17 @@
-// #include <boost/thread.hpp>
-// #include <boost/shared_ptr.hpp>
-// #include <boost/make_shared.hpp>
 #include <iostream>
 #include <chrono>
 #include <vector>
-// #include <cstdlib>
-// #include <ctime>
+#include <cstdlib>
+#include <ctime>
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <cuda_runtime_api.h>
+#include <device_launch_parameters.h>
+#include <device_functions.h>
 
 // nvcc -std=c++11 --compiler-options -Wall -O2 -gencode arch=compute_60,code=compute_60 -o build/vmem mem.cu
 // see also http://llvm.org/docs/CompileCudaWithLLVM.html
+// ref: https://devblogs.nvidia.com/parallelforall/how-implement-performance-metrics-cuda-cc/
 
 // typedef struct {
 // 	boost::shared_ptr<boost::thread> handle;
@@ -16,29 +19,41 @@
 // 	std::size_t ret;
 // } thread_rc;
 
+static std::size_t N_THREADS;
+static std::size_t MEM_LIMIT = 2ul << 30;
+
 void init(int argc, char *argv[]){
-// 	N_THREADS = boost::thread::hardware_concurrency();
+	int deviceCount; cudaDeviceProp deviceProp;
+	cudaError_t cudaResultCode = cudaGetDeviceCount(&deviceCount);
+	if (cudaResultCode != cudaSuccess) std::exit(0);
+	for (int device = 0; device < deviceCount; ++device) {
+		cudaGetDeviceProperties(&deviceProp, device);
+		if (deviceProp.major == 9999) continue;
+		if (device == 0) {
+			printf("GPU processor count = %d\n", deviceProp.multiProcessorCount);
+			printf("Threads per processor = %d\n", deviceProp.maxThreadsPerMultiProcessor);
+			N_THREADS = deviceProp.maxThreadsPerMultiProcessor;
+		}
+	}
 
-// 	if(argc > 1){
-// 		int tmp = atoi(argv[1]);
-// 		if(tmp > 0) MEM_LIMIT = static_cast<std::size_t>(tmp) << 20;
-// 	}
+	if(argc > 1){
+		int tmp = atoi(argv[1]);
+		if(tmp > 0) MEM_LIMIT = static_cast<std::size_t>(tmp) << 20;
+	}
 
-// 	std::cout<<"Page size "<<getpagesize()<<"B"<<std::endl;
-
-// 	MEM_LIMIT /= N_THREADS;
-// 	std::srand(std::time(0));
+	MEM_LIMIT /= N_THREADS;
+	std::srand(std::time(0));
 }
 
 void ramdom_write(std::size_t BLK_SZ){
-// 	std::cout<<"Writing to "<<((MEM_LIMIT*N_THREADS)>>20)<<"MiB block with "<<
-// 		N_THREADS<<" threads (BLK_SZ = "<<(BLK_SZ>>10)<<"KiB)..."<<std::endl;
+	std::cout<<"Writing to "<<((MEM_LIMIT*N_THREADS)>>20)<<"MiB block with "<<
+		N_THREADS<<" threads (BLK_SZ = "<<(BLK_SZ>>10)<<"KiB)..."<<std::endl;
 
 // 	thread_rc threads[N_THREADS];
 // 	for(std::size_t i=0;i<N_THREADS;++i)
 // 		threads[i].local_buffer = new char[MEM_LIMIT];
 
-// 	auto t1 = std::chrono::high_resolution_clock::now();
+	auto t1 = std::chrono::high_resolution_clock::now();
 // 	for(std::size_t i=0;i<N_THREADS;++i){
 // 		threads[i].handle = boost::make_shared<boost::thread>([&,i](){
 // 			std::size_t k = 0;
@@ -49,16 +64,16 @@ void ramdom_write(std::size_t BLK_SZ){
 // 			threads[i].ret = k * BLK_SZ;
 // 		});
 // 	}
-// 	std::size_t total_size = 0;
+	std::size_t total_size = 0;
 // 	for(std::size_t i=0;i<N_THREADS;++i){
 // 		threads[i].handle->join();
 // 		delete[] threads[i].local_buffer;
 // 		total_size += threads[i].ret;
 // 	}
-// 	auto t2 = std::chrono::high_resolution_clock::now();
-// 	std::chrono::duration<double, std::milli> time_ra = t2 - t1;
-// 	std::cout<<"  "<<time_ra.count()<<"ms @ "<<
-// 		static_cast<float>(total_size>>20)/time_ra.count()*1000.f<<"MiB/s"<<std::endl;
+	auto t2 = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double, std::milli> time_ra = t2 - t1;
+	std::cout<<"  "<<time_ra.count()<<"ms @ "<<
+		static_cast<float>(total_size>>20)/time_ra.count()*1000.f<<"MiB/s"<<std::endl;
 }
 
 int main(int argc, char *argv[]){
